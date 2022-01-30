22600cc41ed2e7602795706bb3bdcf377fdc92d5
Vedant Panchal <vedantpanchal12345@gmail.com>
2022-01-30 18:42:41 +0530

    test1

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,10 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Montserrat',
         primaryColor: Colors.white,
         textTheme: const TextTheme(
-          headline1: TextStyle(fontSize: 27.0, fontWeight: FontWeight.w600),
-          headline2: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w600),
+          headline1: TextStyle(fontSize: 27.0),
+          headline2: TextStyle(fontSize: 25.0),
+          headline3: TextStyle(fontSize: 22.0),
+          headline6: TextStyle(fontSize: 15.0),
         ).apply(bodyColor: Colors.white, displayColor: Colors.white),
       ),
       debugShowCheckedModeBanner: false,
