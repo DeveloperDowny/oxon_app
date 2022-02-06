a1fec4e017b93c0b4f96178702f45312d0e43cf9
Vedant Panchal <vedantpanchal12345@gmail.com>
2022-02-07 01:39:57 +0530

    sustainable mapping done for toilets and dustbins; AppTheme added; Minor fixes to CustomAppBar

--- lib/pages/raise_concern.dart
+++ lib/pages/raise_concern.dart
@@ -20,7 +20,7 @@ class _RaiseConcernDirectState extends State<RaiseConcernDirect> {
   Widget build(BuildContext context) {
     return Scaffold(
         drawer: CustomDrawer(),
-        appBar: CustomAppBar(context, "Raise Concern"),
+        appBar: CustomAppBar(context, "Raise a Concern"),
         body: Stack(children: [
           Container(
             decoration: BoxDecoration(
