6e063a6366799a825b16a19fb89c8de225f7dd47
Vedant Panchal <vedantpanchal12345@gmail.com>
2022-02-07 01:30:58 +0530

    after merging into main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:oxon_app/theme/app_theme.dart';
 import 'pages/welcome_pg.dart';
 import './pages/raise_concern.dart';
 // import './pages/raise_concern_camera.dart';
