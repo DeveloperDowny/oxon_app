7ec28fe71704287425daed16b03ca614cbca24ad
Vedant Panchal <vedantpanchal12345@gmail.com>
2022-02-06 20:25:36 +0530

    before plotting toilets marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:oxon_app/theme/app_theme.dart';
 import 'pages/welcome_pg.dart';
 
-void main()  {
-  runApp(MyApp());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
